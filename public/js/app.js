3d5e5666da4027616d5be7b8be5f90708b9bb3b9
metepg <meteg66@gmail.com>
2020-02-17 10:01:53 +0200

    Added styling + Added functionality to take user input and show weather matching the user input

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,39 @@
-console.log('Javascript file loaded')
+// Assigning the form, input and messages to variables
+const locationForm = document.querySelector('form')
+const submitted = document.querySelector('input')
+const firstMessage = document.querySelector('#firstMessage')
+const secondMessage = document.querySelector('#secondMessage')
 
-fetch('http://puzzle.mead.io/puzzle').then( (response) => {
+firstMessage.textContent = '';
+secondMessage.textContent = ''
 
+
+// What happens when you enter address and click submit
+locationForm.addEventListener('submit', (e) => {
+
+    // Prevents browser refreshing
+    e.preventDefault();
+
+    // Input value of the submit field
+    const location = submitted.value;
+
+    // Setting up a loading message while fetching data
+    firstMessage.textContent = 'Loading ...';
+    secondMessage.textContent = '';
+
+    // Get data from the server using fetch()
+    fetch('http://localhost:3000/weather?address=' + location ).then( (response) => {
+    response.json().then((data) => { 
+
+        // If response data contains error display error message (set by the server (me))
+        if (data.error){
+            firstMessage.textContent = data.error
+
+          // Display results
+        } else {
+            firstMessage.textContent =  data.location
+            secondMessage.textContent = data.forecast
+        }
+    })
+})
 })
