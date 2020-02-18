98467c2c5e76539ae99705fb7105fbdb1a44c8dc
metepg <meteg66@gmail.com>
2020-02-18 08:49:12 +0200

    Added new stat from darksky to display on site + fixed a bug where the program would crash if no internet available

--- public/js/app.js
+++ public/js/app.js
@@ -1,39 +1,37 @@
 // Assigning the form, input and messages to variables
-const locationForm = document.querySelector('form')
-const submitted = document.querySelector('input')
-const firstMessage = document.querySelector('#firstMessage')
-const secondMessage = document.querySelector('#secondMessage')
+const locationForm = document.querySelector("form");
+const submitted = document.querySelector("input");
+const firstMessage = document.querySelector("#firstMessage");
+const secondMessage = document.querySelector("#secondMessage");
 
-firstMessage.textContent = '';
-secondMessage.textContent = ''
-
+firstMessage.textContent = "";
+secondMessage.textContent = "";
 
 // What happens when you enter address and click submit
-locationForm.addEventListener('submit', (e) => {
+locationForm.addEventListener("submit", e => {
+  // Prevents browser refreshing
+  e.preventDefault();
 
-    // Prevents browser refreshing
-    e.preventDefault();
+  // Input value of the submit field
+  const location = submitted.value;
 
-    // Input value of the submit field
-    const location = submitted.value;
+  // Setting up a loading message while fetching data
+  firstMessage.textContent = "Loading ...";
+  secondMessage.textContent = "";
 
-    // Setting up a loading message while fetching data
-    firstMessage.textContent = 'Loading ...';
-    secondMessage.textContent = '';
+  // Get data from the server using fetch()
+  fetch("/weather?address=" + location).then(response => {
+    response.json().then(data => {
+      // If response data contains error display error message (set by the server (me))
+      if (data.error) {
+        firstMessage.textContent = data.error;
 
-    // Get data from the server using fetch()
-    fetch('/weather?address=' + location ).then( (response) => {
-    response.json().then((data) => { 
-
-        // If response data contains error display error message (set by the server (me))
-        if (data.error){
-            firstMessage.textContent = data.error
-
-          // Display results
-        } else {
-            firstMessage.textContent =  data.location
-            secondMessage.textContent = data.forecast
-        }
-    })
-})
-})
+        // Display results
+      } else {
+        console.log(data.location);
+        firstMessage.textContent = data.location;
+        secondMessage.textContent = data.forecast;
+      }
+    });
+  });
+});
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,18 +1,34 @@
-const request = require('request');
+const request = require("request");
 
 // Getting the weather from darksky.net based on the input address
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/d2571527da6013a71baeba7cb38dffff/'+ latitude + ',' + longitude +'?units=si&lang=fi';
+  const url =
+    "https://api.darksky.net/forecast/d2571527da6013a71baeba7cb38dffff/" +
+    latitude +
+    "," +
+    longitude +
+    "?units=si&lang=fi";
 
-    request({ url, json: true }, (error, { body }) => {
-        if (error){
-            callback('Unable to connect to weather service!', undefined);
-        } else if (body.error){
-            callback('Unable to find location', undefined);
-        } else {
-            callback(undefined, body.daily.data[0].summary +  ' Tällä hetkellä lämpötila on '+ body.currently.temperature + ' astetta. Sateen mahdollisuus on ' + body.currently.precipProbability + '%' )
-        }
-})
-}
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        body.daily.data[0].summary +
+          " Tällä hetkellä lämpötila on " +
+          body.currently.temperature +
+          " astetta. Sateen mahdollisuus on " +
+          body.currently.precipProbability +
+          "%." +
+          " Päivän ylin lämpötila on " +
+          body.daily.data[0].temperatureHigh +
+          " astetta"
+      );
+    }
+  });
+};
 
-module.exports = forecast
+module.exports = forecast;
